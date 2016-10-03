dc7a8d87317fdad172e6d43f9ff7eaf38613fb1e
simonzlin <simonzlin@gmail.com>
2016-10-02 22:48:09 -0400

    added base and RequestReply

---
+++ src/main/scala/future/FutureDriver.scala
@@ -0,0 +1,62 @@
+package future
+
+/**
+  * Created by zhilin on 10/2/16.
+  */
+object FutureDriver extends App{
+
+  import scala.concurrent.Future
+  import scala.concurrent.ExecutionContext.Implicits.global
+  import scala.util.{Success, Failure}
+
+  val sumFuture=Future {
+    //let thread sleep for 1 second
+    Thread.sleep(5000)
+
+    //perform summation
+    12 + 25
+  }
+
+  //only support 2.12
+// val transformSum= sumFuture.transform {
+//   case Success(res) => Success(res * res)
+//   case Failure(ex) => Failure(ex)
+// }
+
+  val exceptionFuture = Future {
+    100/0
+  }
+
+
+  //println(sumFuture.value)
+
+
+
+
+  //success value returned
+  //will return NONE if main thread is not slept
+  sumFuture.foreach(x => println("ForEach "+x+1))
+
+  sumFuture.onComplete {
+    case Success(res) => println("On Complete Callback "+res * res)
+    case Failure(ex) => println(ex)
+
+  }
+
+
+  //success value returned
+  Thread.sleep(6000)
+
+
+
+
+
+
+
+
+  //error result not returned
+  val exceptionValue=exceptionFuture.foreach(x => x)
+
+
+
+}
